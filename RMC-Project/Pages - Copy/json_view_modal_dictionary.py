853ba2e880215c129791b1f5b5739d340e8cacdc
alesolanoc <alejandro.solano.38@gmail.com>
2024-02-02 15:36:06 -0400

    02

--- RMC-Project/Pages - Copy/json_view_modal_dictionary.py
+++
@@ -1,9 +0,0 @@
-from selenium.webdriver.common.by import By
-
-class jsonviewPage():
-    locator_dictionary = {
-        "json_view_modal" : (By.CSS_SELECTOR, 'textarea.ace_text-input'),
-        "close_json_view" : (By.CSS_SELECTOR, 'div.bx--modal-container.widerModal > div.bx--modal-header > button.bx--modal-close > svg.bx--modal-close__icon > path'),
-        "save_button" : (By.ID, 'save-edit-btn')
-    }
-
--- RMC-Project/Pages - Copy/remove_contributor_popup_dictionary.py
+++
@@ -1,7 +0,0 @@
-from selenium.webdriver.common.by import By
-
-class removeContritutorPage():
-    locator_dictionary = {
-        "confirm_remove_button" : (By.CSS_SELECTOR, 'button.bx--btn.bx--btn--danger--primary.myBtn')
-    }
-
